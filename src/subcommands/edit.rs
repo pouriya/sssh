dcff56a45aedfe0d69c69e1a2532548600b1da24
Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2023-03-03 20:21:20 +0330

    feat: ensure editor

--- src/subcommands/edit.rs
+++ src/subcommands/edit.rs
@@ -5,8 +5,9 @@ use std::path::PathBuf;
 
 pub fn run(settings: &mut Settings) -> Result<(), AppError> {
     settings.ensure_configuration_file()?;
+    settings.ensure_editor_command()?;
     let command = PathBuf::from(settings.editor_command.clone());
-    let argument_list = [settings.configuration_file.clone()].to_vec();
+    let argument_list = settings.editor_argument_list.clone();
     let _ = run_command("Editor", command, argument_list, Vec::new())?;
     settings.ensure_configuration()?;
     Ok(())
--- src/subcommands/sssh.sh
+++ src/subcommands/sssh.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env sh
 
+# `sssh` runs this script with the following arguments:
+#    /path/to/this/script "<USERNAME>@<HOSTNAME>" "<USERNAME>" "<HOSTNAME>" "<PORT>" "<DEBUG>"
+# It also sets the following environment variables:
+#    SSSH_ADDRESS  = "<USERNAME>@<HOSTNAME>"
+#    SSSH_USERNAME = "<USERNAME>"
+#    SSSH_HOSTNAME = "<HOSTNAME>"
+#    SSSH_PORT     = "<PORT>"
+#    SSSH_DEBUG    = "<DEBUG>"
+# If `sssh` itself is started with --verbose (or -v), <DEBUG> will be "1", otherwise "0"
+
 set -e
-if [ "${DEBUG}" = "1" ]; then
-  echo "Arguments:" "$@"
-  echo "ADDRESS:"   "${ADDRESS}"
-  echo "USERNAME:"  "${USERNAME}"
-  echo "HOSTNAME:"  "${HOSTNAME}"
-  echo "PORT:"      "${PORT}"
+if [ "${SSSH_DEBUG}" = "1" ]; then
+  # `sssh` forwards its own logging messages to `stderr`.
+  # We recommend you to do the same here.
+  echo "Arguments:     " "$@"               >&2
+  echo "SSSH_ADDRESS:  " "${SSSH_ADDRESS}"  >&2
+  echo "SSSH_USERNAME: " "${SSSH_USERNAME}" >&2
+  echo "SSSH_HOSTNAME: " "${SSSH_HOSTNAME}" >&2
+  echo "SSSH_PORT:     " "${SSSH_PORT}"     >&2
   set -xe
 fi
 
-ssh -p "${PORT}" "${USERNAME}@${HOSTNAME}"
+ssh -p "${SSSH_PORT}" "${SSSH_ADDRESS}"
